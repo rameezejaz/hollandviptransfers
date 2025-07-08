70e4cff7e0ef45d3ff4765ed028cad5f2f97ab9c
hviphost01 <hvipthost@gmail.com>
2025-07-08 16:19:38 +0200

    Creating a sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.hollandviptransfers.com',
+    generateRobotsTxt: true,
+    sitemapSize: 1000,
+    changefreq: 'monthly',
+    priority: 0.74,
+  };
